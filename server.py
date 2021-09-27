946517f3ec9ff9486cdcba37e49dc22ab063eb8d
Patrick Ofili <patrickofili16@gmail.com>
2021-09-27 23:37:59 +0100

    tidy up

--- server.py
+++ server.py
@@ -12,22 +12,34 @@ def start():
 @app.route('/api/results', methods=["POST"])
 def results():
     req_data = request.get_json()
-    print(req_data)
-    print((req_data["flowRate"]))
+    pi = math.pi   
     # Internal Diameter  (using continuity  equation)
+    # L = straightLength = 100
+    # Q = flowRate = 300
+    # v = velocity = 2.5
+    # p = density = 1000
+    # u= viscosity =0.001
+    # E = roughness = 0.4
+    L = straightLength = float(req_data["straightLength"])
     Q = flowRate = float(req_data["flowRate"])
+    v = velocity = float(req_data["velocity"]) 
     p = density = float(req_data["density"])
     u= viscosity = float(req_data["viscosity"])
-    V = pumpDischarge = float(req_data["pumpDischarge"])
     E = roughness = float(req_data["roughness"])
     docText = req_data['docText']
-    pi = math.pi
-
-    #Internal diameter
-    D = internal_diameter = math.sqrt((4 * Q) / (pi * V)) * 1000
-    V = 2.571
-    A = area = ((pi) * ((D / 2) ** 2)) / 1000000
-    Re = reynolds_number = ((D * V * p) / u) /1000
+    Q = Q/3600
+    D = internal_diameter = math.sqrt((4*Q)/(v*pi))
+    internal_diameter = internal_diameter * 1000
+    internal_diameter = 203.2
+    schedule40 = []
+    A = area = ((pi) * ((internal_diameter/ 2000) ** 2))  * 100
+    new_v = Q * A
+    area = new_v
+    print(new_v, "kkk")
+    Re = reynolds_number = ((D * new_v * p) / u) * 10
+    f = friction_factor = 0.024
+    g = 9.81
+    Dp = pressure_drop = f * (L/(10 * internal_diameter)) * (p/g) * ((new_v**2)/2) * 100
    
     #FlowType
     flowType = ""
@@ -48,6 +60,8 @@ def results():
         "reynolds_number": str(round(reynolds_number,4)),
         "flow_type": str(flowType),
         "relative_roughness":str(round(relative_roughness, 4)),
+        "pressure_drop": str(round(pressure_drop, 4)),
+        "friction_factor": str(round(friction_factor, 4)),
         "docText": docText
         
     }

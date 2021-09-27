55dbc77c77fe8bd70830a827153043f36865f515
Patrick Ofili <patrickofili16@gmail.com>
2021-09-27 13:44:04 +0100

    round values

--- server.py
+++ server.py
@@ -8,12 +8,12 @@ CORS(app)
 @app.route('/', methods=["GET"])
 def start():
     return "<h1>Flask is running </h1>"
-    # return jsonify('flask is running')
    
 @app.route('/api/results', methods=["POST"])
 def results():
-   
     req_data = request.get_json()
+    print(req_data)
+    print((req_data["flowRate"]))
     # Internal Diameter  (using continuity  equation)
     Q = flowRate = float(req_data["flowRate"])
     p = density = float(req_data["density"])
@@ -25,8 +25,6 @@ def results():
 
     #Internal diameter
     D = internal_diameter = math.sqrt((4 * Q) / (pi * V)) * 1000
-    #get next available diameter and then calculate new V but for now continue
-    # D = 0.2032
     V = 2.571
     A = area = ((pi) * ((D / 2) ** 2)) / 1000000
     Re = reynolds_number = ((D * V * p) / u) /1000
@@ -45,11 +43,11 @@ def results():
     relative_roughness = E / D
 
     return {
-        "internal_diameter": str(internal_diameter),
-        "area": str(area),
-        "reynolds_number": str(reynolds_number),
-        "flow_type": flowType,
-        "relative_roughness": str(relative_roughness),
+        "internal_diameter": str(round(internal_diameter, 4)),
+        "area": str(round(area,4)),
+        "reynolds_number": str(round(reynolds_number,4)),
+        "flow_type": str(flowType),
+        "relative_roughness":str(round(relative_roughness, 4)),
         "docText": docText
         
     }
@@ -73,4 +71,80 @@ if __name__ == '__main__':
 # print(dataDict)
 # print(data['schedule'])
  # print(request.data)
+ 
+#  from flask import Flask, request, jsonify
+# from flask_cors import CORS, cross_origin
+# import math
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/', methods=["GET"])
+# def start():
+#     return "<h1>Flask is running </h1>"
+   
+# @app.route('/api/results', methods=["POST"])
+# def results():
+#     req_data = request.get_json()
+#     print(req_data)
+#     print((req_data["flowRate"]))
+#     # Internal Diameter  (using continuity  equation)
+#     Q = flowRate = float(req_data["flowRate"])
+#     p = density = float(req_data["density"])
+#     u= viscosity = float(req_data["viscosity"])
+#     V = pumpDischarge = float(req_data["pumpDischarge"])
+#     E = roughness = float(req_data["roughness"])
+#     docText = req_data['docText']
+#     pi = math.pi
+
+#     #Internal diameter
+#     D = internal_diameter = math.sqrt((4 * Q) / (pi * V)) * 1000
+#     #get next available diameter and then calculate new V but for now continue
+#     # D = 0.2032
+#     V = 2.571
+#     A = area = ((pi) * ((D / 2) ** 2)) / 1000000
+#     Re = reynolds_number = ((D * V * p) / u) /1000
+   
+#     #FlowType
+#     flowType = ""
+#     if Re < 2000:
+#         flowType = "LAMINAR"
+#     if Re > 4000:
+#         flowType = "TURBULENT"
+
+#     if 2000 < Re < 4000:
+#         flowType = "TRANSIENT"
+
+#     #Relative Roughness
+#     relative_roughness = E / D
+
+#     return {
+#         "internal_diameter": str(internal_diameter),
+#         "area": str(area),
+#         "reynolds_number": str(reynolds_number),
+#         "flow_type": flowType,
+#         "relative_roughness": str(relative_roughness),
+#         "docText": docText
+        
+#     }
+
+    
+#     # return {'ok' :  '200'}        
+   
+
+# if __name__ == '__main__':
+#     app.run(debug = True, port = 5000)
+
+
+
+
+
+
+
+
+
+# # dataDict = json.loads(data2)
+# # print(dataDict)
+# # print(data['schedule'])
+#  # print(request.data)
  
